fb189c0a694d4aa8d5b531147b0cd4be8cefa1ca
davidservia <davidserviamorales@gmail.com>
2022-10-10 14:29:51 +0200

    Comprobacion para conducir

--- Practicas/Program.cs
+++ Practicas/Program.cs
@@ -10,12 +10,38 @@ namespace Practicas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Introduzca una medida de un radio para calcular el area de un circulo");
-            Double radio = Double.Parse(Console.ReadLine());
-            Double area = Math.PI * Math.Pow(radio, 2);
-            Console.WriteLine($"El area de la circunferencia es {area}  y el radio introducido es: {radio}");
-
-
+            #region Practica2
+            Console.WriteLine("Introduzca tu edad, por favor");
+            int edad = int.Parse(Console.ReadLine());
+            if (edad >= 18)
+            {
+                Console.WriteLine("Puede conducir, tiene carnet?");
+                String siono;
+                siono = Console.ReadLine();
+                if (siono.Equals("si"))
+                {
+                    Console.WriteLine("Confirmado, puedes conducir");
+                }
+                else if (siono.Equals("no"))
+                {
+                    Console.WriteLine("No puedes conducir");
+                }
+            }
+            else if (edad < 18)
+            {
+                Console.WriteLine("No puedes conducir ");
+            }
+            #endregion
         }
+        #region Practica1
+        /* Console.WriteLine("Introduzca una medida de un radio para calcular el area de un circulo");
+             Double radio = Double.Parse(Console.ReadLine());
+             Double area = Math.PI * Math.Pow(radio, 2);
+             Console.WriteLine($"El area de la circunferencia es {area}  y el radio introducido es: {radio}");
+        */
+        #endregion
+
+
+
     }
 }
